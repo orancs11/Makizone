efc74ea230be08ac0aaadb73e7c1014315653584
orann11 <clippingsipping@gmail.com>
2026-01-24 21:31:29 +0300

    Setup Commit
    First commit setups the Spring environment and add Readmemd file

---
+++ src/main/java/com/Oran/Makizone/MakizoneApplication.java
@@ -0,0 +1,13 @@
+package com.Oran.Makizone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakizoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakizoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Oran/Makizone/MakizoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oran.Makizone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakizoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b1cb36115ae46e0f3d29dc476ff484907f48d201
orann11 <clippingsipping@gmail.com>
2026-01-27 00:18:47 +0300

    Set up database local credentials and db table model is created
    Table category is created as class category using JPA Hibernate. springproperties now holds the local postgresql informations.

--- src/main/java/com/Oran/Makizone/MakizoneApplication.java
+++ src/main/java/com/Oran/Makizone/MakizoneApplication.java
@@ -1,14 +1,16 @@
 package com.Oran.Makizone;
 
+import org.hibernate.SessionBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MakizoneApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(MakizoneApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MakizoneApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/com/Oran/Makizone/Model/Category.java
@@ -0,0 +1,91 @@
+package com.Oran.Makizone.Model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Category table representation in JPA
+
+ */
+
+@Entity
+@Table(schema="Makizone-Catalog", name="category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "slug", nullable = false, unique = true)
+    private String slug;
+    @Column(name = "description", nullable = true, columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+    @OneToMany(mappedBy="parent", cascade= CascadeType.ALL)
+    private List<Category> children = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public List<Category> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Category> children) {
+        this.children = children;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", slug='" + slug + '\'' +
+                ", description='" + description + '\'' +
+                ", parent=" + parent +
+                ", children=" + children +
+                '}';
+    }
+}
